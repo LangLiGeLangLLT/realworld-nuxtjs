47797c6f040a3ec876e4c5f8a94694be583f5242
LLT <418797644@qq.com>
2021-07-18 22:21:35 +0800

    init

---
+++ api/article.js
@@ -0,0 +1,56 @@
+import { request } from '@/plugins/request'
+
+// 获取公共文章列表
+export const getArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/api/articles',
+    params
+  })
+}
+
+// 获取关注的用户文章列表
+export const getFeedArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/api/articles/feed',
+    // Authorization: Token jwt.token.here
+    // headers: {
+    //   // 注意数据格式：Token空格数据Token
+    //   Authorization: `Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTgyNzAxLCJ1c2VybmFtZSI6ImFkbWluMTYzQDE2My5jb20iLCJleHAiOjE2MzE3NzYxMjJ9.w2iZLAfkgT29XhuunhQH7ugwQi_4bkHmQyo7VU9t1KU`
+    // },
+    params
+  })
+}
+
+// 添加点赞
+export const addFavorite = slug => {
+  return request({
+    method: 'POST',
+    url: `/api/articles/${slug}/favorite`
+  })
+}
+
+// 取消点赞
+export const deleteFavorite = slug => {
+  return request({
+    method: 'DELETE',
+    url: `/api/articles/${slug}/favorite`
+  })
+}
+
+// 获取文章详情
+export const getArticle = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}`
+  })
+}
+
+// 获取文章评论
+export const getComments = slug => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}/comments`
+  })
+}
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  router: {
+    linkActiveClass: 'active',
+    // 自定义路由表规则
+    extendRoutes(routes, resolve) {
+      // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
+      routes.length = 0
+
+      routes.push(...[
+        {
+          path: '/',
+          component: resolve(__dirname, 'pages/layout/'),
+          children: [
+            {
+              path: '', // 默认子路由
+              name: 'home',
+              component: resolve(__dirname, 'pages/home/')
+            },
+            {
+              path: '/login',
+              name: 'login',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/register',
+              name: 'register',
+              component: resolve(__dirname, 'pages/login/')
+            },
+            {
+              path: '/profile/:username',
+              name: 'profile',
+              component: resolve(__dirname, 'pages/profile/')
+            },
+            {
+              path: '/settings',
+              name: 'settings',
+              component: resolve(__dirname, 'pages/settings/')
+            },
+            {
+              path: '/editor',
+              name: 'editor',
+              component: resolve(__dirname, 'pages/editor/')
+            },
+            {
+              path: '/article/:slug',
+              name: 'article',
+              component: resolve(__dirname, 'pages/article/')
+            }
+          ]
+        }
+      ])
+    }
+  },
+
+  server: {
+    host: '0.0.0.0', // 生产环境监听所有网卡，localhost 无法提供对外访问
+    port: 3000
+  },
+
+  // 注册插件
+  plugins: [
+    '~/plugins/request.js',
+    '~/plugins/dayjs.js'
+  ]
+}
